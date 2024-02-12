9d768d6ae45a1704fa3263fc38bfd33673694f05
Natallia <nattallius@gmail.com>
2024-02-12 13:57:28 +0100

    tests: implement basic jest tests

--- src/04-test-class/index.test.ts
+++ src/04-test-class/index.test.ts
@@ -1,44 +1,91 @@
-// Uncomment the code below and write your tests
-// import { getBankAccount } from '.';
+import {
+  getBankAccount,
+  InsufficientFundsError,
+  SynchronizationFailedError,
+  TransferFailedError,
+} from '.';
+import lodash from 'lodash';
 
 describe('BankAccount', () => {
+  const initialBalance = 1000;
+  let bankAccount = getBankAccount(initialBalance);
+  let destinationAccount = getBankAccount(initialBalance);
+
+  beforeAll(() => {
+    bankAccount = getBankAccount(initialBalance);
+    destinationAccount = getBankAccount(initialBalance);
+  });
+
   test('should create account with initial balance', () => {
-    // Write your test here
+    expect(bankAccount.getBalance()).toBe(initialBalance);
   });
 
   test('should throw InsufficientFundsError error when withdrawing more than balance', () => {
-    // Write your test here
+    expect(() => bankAccount.withdraw(initialBalance + 1000)).toThrowError(
+      InsufficientFundsError,
+    );
   });
 
   test('should throw error when transferring more than balance', () => {
-    // Write your test here
+    expect(() =>
+      bankAccount.transfer(initialBalance + 1000, destinationAccount),
+    ).toThrowError(InsufficientFundsError);
   });
 
   test('should throw error when transferring to the same account', () => {
-    // Write your test here
+    expect(() =>
+      bankAccount.transfer(initialBalance - 500, bankAccount),
+    ).toThrowError(TransferFailedError);
   });
 
   test('should deposit money', () => {
-    // Write your test here
+    const deposit = 500;
+    const currentBalance = bankAccount.getBalance();
+    expect(bankAccount.deposit(deposit).getBalance()).toBe(
+      currentBalance + deposit,
+    );
   });
 
   test('should withdraw money', () => {
-    // Write your test here
+    const withdraw = 200;
+    const currentBalance = bankAccount.getBalance();
+    expect(bankAccount.withdraw(withdraw).getBalance()).toBe(
+      currentBalance - withdraw,
+    );
   });
 
   test('should transfer money', () => {
-    // Write your test here
+    const transfer = 200;
+    const currentBalance = bankAccount.getBalance();
+    expect(
+      bankAccount.transfer(transfer, destinationAccount).getBalance(),
+    ).toBe(currentBalance - transfer);
+
+    expect(destinationAccount.getBalance()).toBe(initialBalance + transfer);
   });
 
   test('fetchBalance should return number in case if request did not failed', async () => {
-    // Write your tests here
+    const mockedValue = 400;
+    jest.spyOn(lodash, 'random').mockReturnValue(mockedValue);
+
+    const result = await bankAccount.fetchBalance();
+    expect(result).toBe(mockedValue);
   });
 
   test('should set new balance if fetchBalance returned number', async () => {
-    // Write your tests here
+    const mockedValue = 400;
+    jest.spyOn(lodash, 'random').mockReturnValue(mockedValue);
+
+    await bankAccount.synchronizeBalance();
+    expect(bankAccount.getBalance()).toBe(mockedValue);
   });
 
   test('should throw SynchronizationFailedError if fetchBalance returned null', async () => {
-    // Write your tests here
+    const mockedValue = 0;
+    jest.spyOn(lodash, 'random').mockReturnValue(mockedValue);
+
+    expect(bankAccount.synchronizeBalance()).rejects.toThrowError(
+      SynchronizationFailedError,
+    );
   });
 });
--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,17 +1,52 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import axios from 'axios';
+import { throttledGetDataFromApi } from './index';
+
+jest.mock('axios');
+const mockAxios = axios as jest.Mocked<typeof axios>;
+
+jest.mock('lodash', () => {
+  const originalModule = jest.requireActual('lodash');
+
+  //Mock the default export and named export 'foo'
+  return {
+    __esModule: true,
+    ...originalModule,
+    throttle: jest.fn((cb) => cb),
+  };
+});
 
 describe('throttledGetDataFromApi', () => {
+  beforeEach(() => {
+    mockAxios.create.mockReturnThis();
+  });
+
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const spy = jest.spyOn(mockAxios, 'create');
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+    try {
+      await throttledGetDataFromApi('/relative-path'); // to catch axios.get error
+    } catch (e: any) {
+      console.log(e.message);
+    }
+    expect(spy).toBeCalledWith({ baseURL });
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativePath = '/relative-path';
+    const spy = jest.spyOn(axios.create(), 'get');
+    const mockResponse = { data: 'response' };
+    mockAxios.get.mockResolvedValue(mockResponse);
+    await throttledGetDataFromApi(relativePath);
+    expect(spy).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const mockResponse = { data: 'response' };
+    mockAxios.get.mockResolvedValue(mockResponse);
+
+    await expect(throttledGetDataFromApi('/posts')).resolves.toEqual(
+      mockResponse.data,
+    );
   });
 });
